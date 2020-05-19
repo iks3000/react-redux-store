3a3dc440bd4d051a26015d801162b409dff53abc
Stanislav <stanislav.khomyn@skylum.com>
2020-05-18 18:34:26 -0700

    created skeleton React-Redux

--- src/services/bookstore-service.js
+++ src/services/bookstore-service.js
@@ -1,5 +1,16 @@
 export default class BookStoreService {
   getBooks() {
-    return [];
+    return [
+      {
+        id: 1,
+        title: 'Production-Ready Microservices',
+        author: 'Susan J. Fowler'
+      },
+      {
+        id: 2,
+        title: 'Released It!',
+        author: 'Michael T. Nygard'
+      }
+    ];
   }
 };
