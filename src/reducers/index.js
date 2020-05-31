e1074e2d3a9bdcd21ab2fa232b715cd8c322d2a8
Stanislav <stanislav.khomyn@skylum.com>
2020-05-30 19:17:06 -0700

    update element in array

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -37,19 +37,43 @@ const reducer = (state = initialState, action) => {
     case 'BOOK_ADDED_TO_CART':
       const bookId = action.payload;
       const book = state.books.find((book) => book.id === bookId);
-      const newItem = {
-        id: book.id,
-        name: book.title,
-        count: 1,
-        total: book.price
+      const itemIndex = state.cartItems.findIndex(({ id }) => id === bookId);
+      const item = state.cartItems[itemIndex];
+
+      let newItem;
+      if (item) {
+        newItem = {
+          ...item,
+          count: item.count + 1,
+          total: item.total + book.price
+        };
+      } else {
+        newItem = {
+          id: book.id,
+          title: book.title,
+          count: 1,
+          total: book.price
+        };
       };
-      return {
-        ...state,
-        cartItems: [
-          ...state.cartItems,
-          newItem
-        ]
-      };
+
+      if (itemIndex < 0) {
+        return {
+          ...state,
+          cartItems: [
+            ...state.cartItems,
+            newItem
+          ]
+        };
+      } else {
+        return {
+          ...state,
+          cartItems: [
+            ...state.cartItems.slice(0, itemIndex),
+            newItem,
+            ...state.cartItems.slice(itemIndex + 1),
+          ]
+        };
+      }
 
     default:
       return state;
