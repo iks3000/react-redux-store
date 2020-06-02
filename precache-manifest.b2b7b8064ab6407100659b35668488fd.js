ee15e587526728636dda9dad4a8f7f317cadfd53
Stanislav <stanislav.khomyn@skylum.com>
2020-06-01 20:25:39 -0700

    Updates

---
+++ precache-manifest.b2b7b8064ab6407100659b35668488fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "799bf2052f81046748255188eef8e05e",
+    "url": "/react-redux-store/index.html"
+  },
+  {
+    "revision": "7a1c69743697855f2f2e",
+    "url": "/react-redux-store/static/css/main.4c444d65.chunk.css"
+  },
+  {
+    "revision": "af2afe195f8f5f9da2b7",
+    "url": "/react-redux-store/static/js/2.c37b75c0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-redux-store/static/js/2.c37b75c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a1c69743697855f2f2e",
+    "url": "/react-redux-store/static/js/main.7ab7e6de.chunk.js"
+  },
+  {
+    "revision": "860e6f0904ed6f8ee800",
+    "url": "/react-redux-store/static/js/runtime-main.36d85c7c.js"
+  }
+]);
